f7033102e7765e0add8f4cde0116443ff3259dfe
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-05-28 09:01:29 +0300

    drawer

--- layouts/partials/navbar.vue
+++ layouts/partials/navbar.vue
@@ -1,20 +1,34 @@
 <template>
   <nav>
-    <v-app-bar app>
+    <v-app-bar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>
         <span class="font-weight-light">Todo</span>
         <span>Vuetify</span>
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
     </v-app-bar>
+
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      app
+      class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
-
 <script>
     export default {
-        name: "Navbar"
+        name: "Navbar",
+      data() {
+          return {
+            drawer: false
+          }
+      }
     }
 </script>
-
-<style scoped>
-
-</style>
