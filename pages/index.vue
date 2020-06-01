029bff0d6373e0b7523318f42d1608a4dafaf954
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-06-01 09:12:33 +0300

    more styling

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
 
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atque aut autem beatae commodi eveniet mollitia, unde. Aspernatur consequatur exercitationem explicabo incidunt, nihil numquam odio officiis omnis qui quo! At.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi eligendi in ipsam iusto optio unde ut veniam, voluptate! Aliquid dolorem ea ipsum magnam magni nostrum officia praesentium quibusdam sint ut?</p>
+
       </div>
     </v-flex>
   </v-layout>
--- pages/styling.vue
+++ pages/styling.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <v-layout>
     <v-flex class="text-center">
       <div class="styling">
@@ -23,6 +24,32 @@
       </div>
     </v-flex>
   </v-layout>
+
+  <v-layout row wrap class="my-5">
+    <v-flex xs12 md6>
+      <v-btn outline block class="primary">1</v-btn>
+    </v-flex>
+    <v-flex xs4 md2>
+      <v-btn outline block class="info">2</v-btn>
+    </v-flex>
+    <v-flex xs4 md2>
+      <v-btn outline block class="info">2</v-btn>
+    </v-flex>
+    <v-flex xs4 md2>
+      <v-btn outline block class="info">2</v-btn>
+    </v-flex>
+  </v-layout>
+
+    <v-layout row wrap justify-space-around>
+      <v-flex xs4 md3>
+        <v-btn outline block class="success">1</v-btn>
+      </v-flex>
+      <v-flex xs4 md3>
+        <v-btn outline block class="success">2</v-btn>
+      </v-flex>
+    </v-layout>
+
+  </div>
 </template>
 
 <script>
