bc7f1776bd39710aa3b8303a94d01fbe4ed903fa
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-06-06 14:11:59 +0300

    form initial setup

--- pages/form/index.vue
+++ pages/form/index.vue
@@ -1,19 +1,70 @@
 <template>
-  <v-layout>
-    <v-flex>
-      <div class="forms">
-        <h1 class="grey--text">Forms Examples</h1>
-      </div>
-    </v-flex>
-  </v-layout>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Personal details</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-code-tags</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="First Name"
+                    name="first_name"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="first_name"
+                    label="First Name"
+                    name="first_name"
+                    prepend-icon="mdi-lock"
+                    type="text"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
 </template>
 
 <script>
-    export default {
-        name: "index"
-    }
+  export default {
+    name: "forms",
+    components: {}
+  }
 </script>
-
-<style scoped>
-
-</style>
