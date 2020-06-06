077b4c9ae97887821d9f0d061c68f932fcc8d4d8
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-06-06 15:33:52 +0300

    forms WIP

---
+++ components/forms/Wrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <slot
+      :group="group"
+      :fields="fields"
+      :validation="validationBag"
+    ></slot>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Wrapper",
+      props: {
+          group: {
+            type: String,
+            required: true
+          },
+          behaviour: {
+            type: String,
+            required: false
+        }
+      },
+      data: () => ({
+        fields: {},
+        validationBag: {}
+      }),
+    }
+</script>
